30592681dfda51c6c78e1c6fc9ceecae663e9932
Nathanael <njluke@cedarville.edu>
2025-10-04 13:29:43 -0400

    Fixed mobile issue maybe

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function setVh() {
-  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-}
-setVh();
-window.addEventListener('resize', setVh);
+document.querySelectorAll('.socials a').forEach(icon => {
+  icon.addEventListener('click', e => {
+    e.preventDefault();
+    icon.style.transition = 'transform 0.2s ease';
+    icon.style.transform = 'scale(1.5)';
+    setTimeout(() => {
+      icon.style.transform = 'scale(1)';
+      window.open(icon.href, '_blank');
+    }, 300);
+  });
+});
+
